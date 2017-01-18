a6dbb5745891104ca937914a4e956bf56fd5aaa1
Henrique Oliveira <henrique.oliveira@am4.com.br>
2017-01-18 11:53:21 -0200

    hello world

---
+++ asp.net-admin-site-template/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp.net_admin_site_template.MvcApplication" Language="C#" %>
---
+++ asp.net-admin-site-template/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp.net_admin_site_template.Startup))]
+namespace asp.net_admin_site_template
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
